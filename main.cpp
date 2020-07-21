f31047843a75b8bc96dc3b01b4c1246d70937406
Johnson <johnsonli1993@gmail.com>
2020-07-21 14:32:26 +0300

    support filters

--- main.cpp
+++ main.cpp
@@ -32,9 +32,15 @@ int PORT = 0;
 int PORT_SRC = 0;
 int PORT_DST = 0;
 uint32_t IP = 0;
+std::string IP_STR;
 uint32_t IP_SRC = 0;
+std::string IP_SRC_STR;
 uint32_t IP_DST = 0;
-std::string PROTOCOL = "";
+std::string IP_DST_STR;
+std::string PROTOCOL;
+bpf_u_int32 NET;
+bpf_u_int32 MASK;
+std::string DEV = "lo";
 std::string LOG_FILE = "network.log";
 
 void on_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
@@ -81,8 +87,8 @@ void on_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *pac
     timestamp = monotime.tv_sec;
   }
   if (monotime.tv_sec > timestamp) {
-    std::cout << "Downlink bandwidth: " << (downloaded - downloaded_pre) / (monotime.tv_sec - timestamp)
-              << " bps, uplink bandwidth: " << (uploaded - uploaded_pre) / (monotime.tv_sec - timestamp)
+    std::cout << "Downlink bandwidth: " << 8 * (downloaded - downloaded_pre) / (monotime.tv_sec - timestamp)
+              << " bps, uplink bandwidth: " << 8 * (uploaded - uploaded_pre) / (monotime.tv_sec - timestamp)
               << " bps, at " << monotime.tv_sec * 1000 + monotime.tv_nsec / 1000000
               << std::endl;
     downloaded_pre = downloaded;
@@ -105,11 +111,11 @@ void arg_parse(int argc, char **argv) {
   po::options_description desc("Allowed options");
   desc.add_options()
           ("help", "print help message")
-          ("log_file", po::value<std::string>(), "set the logger file")
+          ("device", po::value<std::string>(), "set the NIC to capture")
           ("ip", po::value<std::string>(), "filter by IP address")
           ("port", po::value<int>(), "filter by port")
-          ("src_port", po::value<int>(), "filter by src port")
-          ("dst_port", po::value<int>(), "filter by dst port")
+          ("src-port", po::value<int>(), "filter by src port")
+          ("dst-port", po::value<int>(), "filter by dst port")
           ("src", po::value<std::string>(), "filter by source IP address")
           ("dst", po::value<std::string>(), "filter by destination IP address")
           ("protocol", po::value<std::string>(), "filter by transport layer protocol");
@@ -128,93 +134,121 @@ void arg_parse(int argc, char **argv) {
     std::cout << desc << "\n";
     exit(EXIT_SUCCESS);
   }
-  if (vm.count("log_file")) {
-    LOG_FILE = vm["log_file"].as<std::string>();
+  if (vm.count("device")) {
+    DEV = vm["device"].as<std::string>();
   }
-  if (vm.count("ip_src")) {
-    inet_pton(AF_INET, vm["ip_src"].as<std::string>().c_str(), &IP_SRC);
+  if (vm.count("src")) {
+    IP_SRC_STR = vm["src"].as<std::string>();
+    inet_pton(AF_INET, IP_SRC_STR.c_str(), &IP_SRC);
   }
-  if (vm.count("ip_dst")) {
-    inet_pton(AF_INET, vm["ip_dst"].as<std::string>().c_str(), &IP_DST);
+  if (vm.count("dst")) {
+    IP_DST_STR = vm["dst"].as<std::string>();
+    inet_pton(AF_INET, IP_DST_STR.c_str(), &IP_DST);
   }
   if (vm.count("ip")) {
-    inet_pton(AF_INET, vm["ip"].as<std::string>().c_str(), &IP);
+    IP_STR = vm["ip"].as<std::string>();
+    inet_pton(AF_INET, IP_STR.c_str(), &IP);
     if (IP_SRC != 0 || IP_DST != 0) {
       std::cerr << "ip should not be set along with src/dst" << std::endl;
+      exit(EXIT_FAILURE);
     }
   }
-  if (vm.count("src_port")) {
-    PORT_SRC = vm["src_port"].as<int>();
+  if (vm.count("src-port")) {
+    PORT_SRC = vm["src-port"].as<int>();
   }
-  if (vm.count("dst_port")) {
-    PORT_DST = vm["dst_port"].as<int>();
+  if (vm.count("dst-port")) {
+    PORT_DST = vm["dst-port"].as<int>();
   }
   if (vm.count("port")) {
     PORT = vm["port"].as<int>();
     if (PORT_SRC > 0 || PORT_DST > 0) {
       std::cerr << "port should not be set along with src_port/dst_port" << std::endl;
+      exit(EXIT_FAILURE);
     }
   }
   if (vm.count("protocol")) {
     PROTOCOL = vm["protocol"].as<std::string>();
+    std::transform(PROTOCOL.begin(), PROTOCOL.end(), PROTOCOL.begin(), ::toupper);
     if (std::find(std::begin(PROTOCOLS), std::end(PROTOCOLS), PROTOCOL) == std::end(PROTOCOLS)) {
       std::cerr << "Invalid protocol: " << PROTOCOL << std::endl;
       exit(EXIT_FAILURE);
+    }
+    if (PROTOCOL == "ICMP") {
+      if (PORT > 0 || PORT_SRC > 0 || PORT_DST > 0) {
+        std::cerr << "ICMP does not support port parameter" << std::endl;
+        exit(EXIT_FAILURE);
+      }
     }
   }
 }
 
 void setup_filters(pcap_t *handle) {
   bpf_program fp{};      /* compiled filter program (expression) */
+  std::string expr;
+  char net[INET_ADDRSTRLEN];
+  sockaddr_in sa{};
+  sa.sin_addr.s_addr = NET;
+  inet_ntop(AF_INET, &(sa.sin_addr), net, INET_ADDRSTRLEN);
+  if (PROTOCOL.length() > 0) {
+    expr += std::string(expr.length() > 0 ? " and " : "") + "ip proto " + PROTOCOL;
+  }
   if (IP != 0) {
-    char str[INET_ADDRSTRLEN];
-    int res = 0;
-    inet_ntop(AF_INET, &IP, str, INET_ADDRSTRLEN);
-    std::cout << "ip: " << str << std::endl;
-    res = pcap_compile(handle, &fp, "ip", 0, IP);
-    std::cout << res << std::endl;
-    res = pcap_setfilter(handle, &fp);
-    std::cout << res << std::endl;
+    expr += std::string(expr.length() > 0 ? " and " : "") + "ip host " + IP_STR;
+  }
+  if (IP_SRC != 0) {
+    expr += std::string(expr.length() > 0 ? " and " : "") + "src host " + IP_SRC_STR;
+  }
+  if (IP_DST != 0) {
+    expr += std::string(expr.length() > 0 ? " and " : "") + "dst host " + IP_DST_STR;
+  }
+  if (PORT != 0) {
+    expr += std::string(expr.length() > 0 ? " and " : "") + "port " + std::to_string(PORT);
+  }
+  if (PORT_SRC != 0) {
+    expr += std::string(expr.length() > 0 ? " and " : "") + "src port " + std::to_string(PORT_SRC);
+  }
+  if (PORT_DST != 0) {
+    expr += std::string(expr.length() > 0 ? " and " : "") + "dst port " + std::to_string(PORT_DST);
+  }
+  std::cout << "Pcap filter: " << expr << " on NIC: " << net << std::endl;
+  char str[INET_ADDRSTRLEN];
+  int res = 0;
+  inet_ntop(AF_INET, &IP, str, INET_ADDRSTRLEN);
+  res = pcap_compile(handle, &fp, expr.c_str(), 0, NET);
+  if (res != 0) {
+    std::cerr << "Failed to call pcap_compile" << std::endl;
+  }
+  res = pcap_setfilter(handle, &fp);
+  if (res != 0) {
+    std::cerr << "Failed to call pcap_setfilter" << std::endl;
   }
 }
 
 int main(int argc, char **argv) {
   arg_parse(argc, argv);
 
-  std::string dev = "lo";      /* capture device name */
   char err_buf[PCAP_ERRBUF_SIZE];    /* error buffer */
   pcap_t *handle;        /* packet capture handle */
   int num_packets = 0;      /* number of packets to capture */
   bpf_program fp{};      /* compiled filter program (expression) */
-  char filter_exp[] = "ip";    /* filter expression [3] */
-  bpf_u_int32 mask;      /* subnet mask */
-  bpf_u_int32 net;      /* subnet address */
 
-  if (pcap_lookupnet(dev.c_str(), &net, &mask, err_buf) == -1) {
-    std::cerr << "Couldn't get netmask for device " << dev << ": " << err_buf << std::endl;
-    net = 0;
-    mask = 0;
+  if (pcap_lookupnet(DEV.c_str(), &NET, &MASK, err_buf) == -1) {
+    std::cerr << "Couldn't get netmask for device " << DEV << ": " << err_buf << std::endl;
+    exit(EXIT_FAILURE);
+  } else {
+    std::cout << "Listening on " << DEV << std::endl;
   }
-  my_ip = get_ip(dev);
-  handle = pcap_open_live(dev.c_str(), SNAP_LEN, false, 1, err_buf);
+  my_ip = get_ip(DEV);
+  handle = pcap_open_live(DEV.c_str(), SNAP_LEN, false, 1, err_buf);
   if (handle == nullptr) {
-    std::cerr << "Couldn't open device " << dev << ": " << err_buf << std::endl;
+    std::cerr << "Couldn't open device " << DEV << ": " << err_buf << std::endl;
     exit(EXIT_FAILURE);
   }
   if (pcap_datalink(handle) != DLT_EN10MB) {
-    std::cerr << dev << " is not an Ethernet device" << std::endl;
+    std::cerr << DEV << " is not an Ethernet device" << std::endl;
     exit(EXIT_FAILURE);
   }
-//  setup_filters(handle);
-  if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
-    fprintf(stderr, "Couldn't parse filter %s: %s\n",
-            filter_exp, pcap_geterr(handle));
-    exit(EXIT_FAILURE);
-  }
-  if (pcap_setfilter(handle, &fp) == -1) {
-    std::cerr << "Couldn't install filter " << filter_exp << ": " << pcap_geterr(handle) << std::endl;
-    exit(EXIT_FAILURE);
-  }
+  setup_filters(handle);
   pcap_loop(handle, num_packets, on_packet, nullptr);
 
   pcap_freecode(&fp);
